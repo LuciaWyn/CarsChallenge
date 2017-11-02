8c6667911b887a97a80e47047f192502dce418a5
LuciaWyn <32106003+LuciaWyn@users.noreply.github.com>
2017-11-02 12:22:24 +0000

    Create head.php

---
+++ app/view/partial/head.php
@@ -0,0 +1,18 @@
+<head>
+	<meta charset="utf-8">
+
+	<title><?php echo (isset($title)? $title : 'Canidate Assesment');?></title>
+	<meta name="description" content="<?php echo (isset($description) ? $description : '');?>">
+	<meta name="author" content="Screenpages">
+
+
+	<script src="/js/jquery-1.11.2.js"></script>
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--Stylesheets-->
+	<link rel="stylesheet" href="css/styles.css?v=1.0">
+	<link rel="stylesheet" href="css/mystyles.css?v=1.0">
+
+</head>
